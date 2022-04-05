3a36fc6d6189276566dc145afb3b95ece50fbf7c
zhangjun <ewalker@live.cn>
2022-04-05 15:15:42 +0000

    Committing clang-format changes

--- c++/modern_cpp/c++11/future/run_future.cc
+++ c++/modern_cpp/c++11/future/run_future.cc
@@ -1,25 +1,25 @@
-#include <iostream>
-#include <future>
-#include <thread>
 #include <chrono>
+#include <future>
+#include <iostream>
+#include <thread>
 
 void run(int val) {
-    std::this_thread::sleep_for(std::chrono::seconds(3));
-    std::cout << "run tid: " << std::this_thread::get_id() << ", val: " << val << std::endl;
+  std::this_thread::sleep_for(std::chrono::seconds(3));
+  std::cout << "run tid: " << std::this_thread::get_id() << ", val: " << val
+            << std::endl;
 }
 
-struct runner {
-};
+struct runner {};
 
 int main() {
-    std::cout << "main thread, tid: " << std::this_thread::get_id() << std::endl;
-    std::future<void> future = std::async(std::launch::async, run, 4);
-    future.get();
+  std::cout << "main thread, tid: " << std::this_thread::get_id() << std::endl;
+  std::future<void> future = std::async(std::launch::async, run, 4);
+  future.get();
 
-    std::future<void> f = std::async(std::launch::async, run, 5);
-    std::future_status status = f.wait_for(std::chrono::seconds(2));
-    if(status == std::future_status::timeout) {
-        std::cout << "timeout." << std::endl;
-    }
-    return 0;
+  std::future<void> f = std::async(std::launch::async, run, 5);
+  std::future_status status = f.wait_for(std::chrono::seconds(2));
+  if (status == std::future_status::timeout) {
+    std::cout << "timeout." << std::endl;
+  }
+  return 0;
 }
